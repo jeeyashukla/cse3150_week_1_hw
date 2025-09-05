5e019ad06c02ee9928ce0b6d4c9c5436db604948
jeeya shukla <jeeyashukla@jeeyas-MacBook-Pro.local>
2025-09-05 11:12:06 -0400

    inital

---
+++ include/AdvancedMath.h
@@ -0,0 +1,9 @@
+#ifndef ADVANCEDMATH_H
+#define ADVANCEDMATH_H
+#include "MathUtils.h"
+
+namespace AdvancedMath {
+    int square(int n);
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "MathUtils.h"
+#include "AdvancedMath.h"
+
+int main() {
+    int a = 3;
+    int b = 4;
+
+    std::cout << "a+b =" << MathUtils::add(a,b) << std::endl;
+    std::cout <<short "a*b =" << MathUtils::multiply(a,b) << std::endl
+    std::cout << "square(a) =" << AdvancedMath::square(a) << std::endl
+
+    return 0;
+
+}
